0d39112dbd14caaf94c179d972ebaefcc4aa5a2a
ANDREW DAVID SCHWEINFURTH <andrew.schweinfurth@walgreens.com>
2022-11-08 16:46:54 -0600

    fixed config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const domain = process.env.domain || "http://ds-game.onrender.com",
+const domain = process.env.domain || "https://ds-game.onrender.com",
     PORT = process.env.PORT,
     SHOW_LOGS = (process.env.SHOW_LOGS) ? Boolean(process.env.SHOW_LOGS) : false,
     LOG_LEVELS = (process.env.LOG_LEVELS) ? JSON.parse(process.env.LOG_LEVELS) : [];
